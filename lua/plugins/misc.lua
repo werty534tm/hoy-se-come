a0a2008e9fbd84d118d204347dc01ee86a97e071
werty534tm <tomascanosanta@gmail.com>
2024-12-01 21:04:56 +0100

    Esoooo

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,6 +2,24 @@ return {
 	"tpope/vim-sleuth",
 	{ "Bilal2453/luvit-meta", lazy = true },
 	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	{
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup()
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+		end,
+	},
+	{
 		"folke/todo-comments.nvim",
 		event = "VimEnter",
 		dependencies = { "nvim-lua/plenary.nvim" },
