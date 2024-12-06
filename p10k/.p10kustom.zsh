10fa744f2b16e53104ca0c6ebfdc5bc5666a14d1
werty534tm <tomascanosanta@gmail.com>
2024-12-06 13:32:01 +0100

    Añado ulauncher, zsh y p10k

---
+++ p10k/.p10kustom.zsh
@@ -0,0 +1,12 @@
+typeset -g POWERLEVEL9K_DIR_CLASSES=(
+  '~/proyectos_peronales(|/*)'  PROYECTOS       ''
+  '~/universidad(|/*)'          UNIVERSIDAD     ''
+  '~/.config(|/*)'              CONFIG          ''
+  '/tmp(|/*)'                   TEMP            ''
+)
+
+typeset -g POWERLEVEL9K_DIR_PROYECTOS_VISUAL_IDENTIFIER_EXPANSION=' '
+typeset -g POWERLEVEL9K_DIR_CONFIG_VISUAL_IDENTIFIER_EXPANSION=' '
+typeset -g POWERLEVEL9K_DIR_UNIVERSIDAD_VISUAL_IDENTIFIER_EXPANSION=' '
+typeset -g POWERLEVEL9K_DIR_TEMP_VISUAL_IDENTIFIER_EXPANSION=' '
+
---
+++ ulauncher/.config/ulauncher/scripts/randass_dog.py
@@ -0,0 +1,24 @@
+from random import randint
+
+dogs = (
+    "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fstupid-looking-ass-v0-8obvzbm2j3pc1.jpg%3Fwidth%3D1536%26format%3Dpjpg%26auto%3Dwebp%26s%3Ddaf402357e750bc857b72f1b94037f0399322a6d&rdt=51092",
+    "https://media.istockphoto.com/id/1147089701/photo/dumb-crazy-dog.jpg?s=612x612&w=0&k=20&c=2zPiB24L15rV16gcWVvWpPO-tkKqsy_W90aNNx-HE6w=",
+    "https://i.pinimg.com/736x/3f/5e/f0/3f5ef0da35f40cf222776b6eb76e84d4.jpg",
+    "https://media.tenor.com/BqX5vYMRqeQAAAAe/stupid-dog.png",
+    "https://d2edvletk84qg.cloudfront.net/140852/140852_Image557.jpg",
+    "https://static.wikia.nocookie.net/d4e907fa-00cc-413c-83b1-c938ddedc5ba/scale-to-width/755",
+    "https://pbs.twimg.com/media/EexFnRuXYAAdBM4.jpg:large",
+    "https://i.pinimg.com/550x/66/f1/1c/66f11cde305af5be655ead6d664f3284.jpg",
+    "https://i.imgflip.com/99mlsv.jpg",
+    "https://media.tenor.com/9GsBQZZCwF8AAAAM/dumb-dog.gif",
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/92d44925-4d05-4543-ac8c-0d9ec74661fc/d3eixpa-4912b95f-d894-4b1a-94ff-93170aac99ad.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzkyZDQ0OTI1LTRkMDUtNDU0My1hYzhjLTBkOWVjNzQ2NjFmY1wvZDNlaXhwYS00OTEyYjk1Zi1kODk0LTRiMWEtOTRmZi05MzE3MGFhYzk5YWQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9ezaQe0Qw4MADKFHUz4zDpL7cyqyBP6JAriuoKzI890",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ8-uGRglgsLBxGC_J9OCJGOhrPNouU-DTHg&s",
+    "https://media.tenor.com/mNz17DpKv6cAAAAM/dumb-idiot-dog-lifeistoxic.gif",
+    "https://pbs.twimg.com/media/GLV4lhHXgAED7Pf.jpg:large",
+    "https://i.pinimg.com/originals/be/14/49/be1449c75d0b0b97a71ad8df00293a49.jpg",
+    "https://i.pinimg.com/736x/49/db/17/49db17f4545d73845c4cf71c7bf360dc.jpg",
+    "https://i.pinimg.com/736x/44/19/c3/4419c3c2e1e188b928dc480d0853d795.jpg",
+    "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2F62clhbzqnv7c1.jpeg",
+)
+
+print(dogs[randint(0, len(dogs)-1)])
