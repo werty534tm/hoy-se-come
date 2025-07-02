5a3b15355d2f5196e4ed15d95c28354228003c81
werty534tm <tomascanosanta@gmail.com>
2025-07-02 20:34:23 +0200

    añado mi basura (por ahora)

---
+++ basura/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,33 @@
+return {
+	-- "catppuccin/nvim",
+	-- lazy = false,
+	-- name = "catppuccin",
+	-- priority = 1000,
+	-- config = function()
+	-- 	vim.cmd.colorscheme("catppuccin-mocha")
+	-- 	vim.cmd.hi("Comment gui=none")
+	-- end,
+	-- opts = {
+	-- 	transparent = true,
+	-- 	styles = {
+	-- 		sidebars = "transparent",
+	-- 		floats = "transparent",
+	-- 	},
+	-- },
+	-- "AlphaTechnolog/pywal.nvim",
+	-- priority = 1000,
+	-- config = function()
+	-- 	vim.cmd.colorscheme("pywal")
+	-- 	vim.cmd.hi("Comment gui=none")
+	-- end,
+	"ellisonleao/gruvbox.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("gruvbox")
+		vim.cmd.hi("Comment gui=none")
+	end,
+	opts = {
+		transparent_mode = true
+	}
+}
